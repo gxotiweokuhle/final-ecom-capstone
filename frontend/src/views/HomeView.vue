2e07d40b1e46ef94221a59c77b16595e3714d2cd
okuhle gxotiwe <gxotiweokuhle@gmail.com>
2023-09-01 08:20:20 +0200

    styling

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,171 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid p-0">
+       <!--hero-->
+       <section class=" overflow-x-hidden hero m-0 align-content-sm-start">
+              <div class="hero-inner text-center">
+                    <a href="/products" class="btn shadow__btn mb-2">Shop Now!</a>
+              </div>
+            </section>
+
+  <!----featured info-->
+              <section id="featured" class="mt-3">
+              <div>
+                <h2 class="text-black">Featured Categories</h2>
+                <p class="text-black">We offer our Art Collection in different categories of craftmanship.</p>
+              </div>
+            </section>
+
+    <!--categories--->
+          
+               
+    <div class="container mb-4">
+
+        <div class="row">
+        
+          <div class="column">
+            <div class="card">
+              <div class="content">
+                <div class="front">
+                  <img class="profile" width="100%" src="">
+                  <h2 class="mt-4 text-white">Painting</h2>
+                </div>
+                <div class="back from-left">  
+                  <p class="des fw-semibold p-2">
+                  
+                  </p>
+                  <button class="shadow__btn">
+                    <a href="/products" >Shop Now!</a>
+                  </button>
+                </div>
+              </div>
+          </div>
+        </div>
+
+
+          <div class="column">
+            <div class="card">
+              <div class="content">
+                <div class="front">
+                  <img class="profile" width="100%" src="">
+                  <h2 class="mt-4 text-white">Sculpting</h2>
+                </div>
+                <div class="back from-bottom">
+                  <p class="des fw-semibold p-2">
+                  </p>
+                  <button class="shadow__btn">
+                    <a href="/products" >Shop Now!</a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+  
+  
+        <div class="column">
+          <div class="card">
+            <div class="content">
+              <div class="front">
+                <img class="profile" width="100%" src="" >
+                <h2 class="mt-4 text-white">Photography</h2>
+              </div>
+              <div class="back from-right">
+                <p class="des fw-semibold p-2">
+                </p>
+                <button class="shadow__btn">
+                  <a href="/products">Shop Now!</a>
+                  </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+       </div>
+    </div>
+
+
+    <!-- featured info -->
+    <section id="featured" class="mt-3">
+              <div>
+                <h2 class="text-black">Featured Artist</h2>
+                <p class="text-black">Get to know more about our fellow Artists and what inspired them.</p>
+              </div>
+            </section>
+
+            <div class="container mb-4">
+
+    <!-- featured artists -->
+<div class="row">
+
+  <div class="column">
+    <div class="card">
+      <div class="content">
+        <div class="front">
+          <img class="profile" width="100%" src="">
+          <h2 class="mt-4 text-white">Cinga Samson</h2>
+        </div>
+        <div class="back from-left">  
+          <p class="des fw-semibold p-2">
+          
+          </p>
+          <button class="shadow__btn">
+            <a href="/products" >Shop Now!</a>
+          </button>
+        </div>
+      </div>
+  </div>
+</div>
+
+
+  <div class="column">
+    <div class="card">
+      <div class="content">
+        <div class="front">
+          <img class="profile" width="100%" src="">
+          <h2 class="mt-4 text-white">Mary Sibande</h2>
+        </div>
+        <div class="back from-bottom">
+          <p class="des fw-semibold p-2">
+          </p>
+          <button class="shadow__btn">
+            <a href="/products" >Shop Now!</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<div class="column">
+  <div class="card">
+    <div class="content">
+      <div class="front">
+        <img class="profile" width="100%" src="" >
+        <h2 class="mt-4 text-white">Sanele Matsolo</h2>
+      </div>
+      <div class="back from-right">
+        <p class="des fw-semibold p-2">
+        </p>
+        <button class="shadow__btn">
+          <a href="/products">Shop Now!</a>
+          </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+</div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
+<style scoped>
+
+</style>
